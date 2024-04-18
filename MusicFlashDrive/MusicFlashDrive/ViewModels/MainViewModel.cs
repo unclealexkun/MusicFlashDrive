69bacbe3763928f307cdb40bc8f5d422df9dc2b6
UncleAlexKun <raisin27@mail.ru>
2024-04-18 23:47:09 +0400

    Пустой проект

--- MusicFlashDrive/MusicFlashDrive/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MusicFlashDrive.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- MusicFlashDrive/MusicFlashDrive/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace MusicFlashDrive.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
